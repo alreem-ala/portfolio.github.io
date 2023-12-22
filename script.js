f5dfcd5eb94a3c7bbbc3e6f0f39a9553b6f6abd5
alreem-ala <123474036+alreem-ala@users.noreply.github.com>
2023-12-22 16:03:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'My Portfolio'; /* The text */
+var txt = 'Alreems Portfolio'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
