baef3711c0c713b93b963ceb625d7e85ff6a035f
alreem-ala <123474036+alreem-ala@users.noreply.github.com>
2023-12-22 15:39:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'Welcome to'; /* The text */
+var txt = 'Alreem'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
